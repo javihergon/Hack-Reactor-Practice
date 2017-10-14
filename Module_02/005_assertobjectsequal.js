4340c996b2a97d7b422c605df31a28a4fc2aeca8
Javier Gonzalez <javihergonzalez@gmail.com>
2017-10-13 17:16:02 -0700

    Add files via upload

---
+++ Module_02/005_assertobjectsequal.js
@@ -0,0 +1,56 @@
+/*
+Write an "assertObjectsEqual" function from scratch.
+
+Assume that the objects in question contain only scalar values (i.e., simple values like strings or numbers).
+
+It is OK to use JSON.stringify().
+
+Note: The examples below represent different use cases for the same test. In practice, you should never have multiple tests with the same name.
+
+Examples
+
+SUCCESS CASE
+
+var person = {
+  firstName: 'Joe',
+  lastName: 'Blow'
+};
+person = updateObject(person, 'firstName', 'Jack');
+
+var expected = {
+  firstName: 'Jack',
+  lastName: 'Blow'
+};
+
+assertObjectsEqual(person, expected, 'updates person's existing first name field');
+// console output:
+// passed
+
+FAILURE CASE
+
+var person = {
+  firstName: 'Joe',
+  lastName: 'Blow'
+};
+person = updateObject(person, 'age', 35);
+
+var expected = {
+  firstName: 'Joe',
+  lastName: 'Blow',
+  age: 35
+};
+assertObjectsEqual(person, expected, 'adds person's non-existing age field');
+// console output:
+// FAILED [adds person's non-existing age field] Expected {"firstName":"Joe", "lastName":"Blow", age:35}, but got {"firstName":"Joe", "lastName":"Blow"}
+
+*/
+
+function assertObjectsEqual(actual, expected, testName) {
+   actual = JSON.stringify(actual);
+   expected = JSON.stringify(expected);
+   if(actual === expected) {
+     console.log('passed');
+  } else {
+     console.log('FAILED [' + testName + '] Expected ' + expected + ', but got ' + actual);
+  }
+}
