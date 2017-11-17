9644d041fffe0d487221445e71445d8a4587ee21
Javier Gonzalez <javihergonzalez@gmail.com>
2017-11-16 20:06:51 -0800

    Add files via upload

---
+++ Module_01/075_removeoddvalues.js
@@ -0,0 +1,25 @@
+/*
+Write a function called "removeOddValues".
+
+Given an object, "removeOddValues" removes any properties whose valuse are odd numbers.
+
+var obj = {
+  a: 2,
+  b: 3,
+  c: 4
+};
+removeOddValues(obj);
+console.log(obj); // --> { a: 2, c: 4 }
+*/
+
+function removeOddValues(obj) {
+  // your code here
+  for (var key in obj) {
+  	if (typeof obj[key] === "number") {
+  		if (obj[key] % 2 !== 0) {
+  			delete obj[key];
+  		}
+  	}
+  }
+  return obj;
+}
